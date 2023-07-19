dbd4317d1cc489917929a42b1e929c914cdf08f6
S. Sergey <SS86RG@ya.ru>
2023-07-19 05:26:46 +0300

    save 9-3

---
+++ P_9_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет
+// все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 5 -> "2, 4"
+//M = 4; N = 8 -> "4, 6, 8"
+
+int M = ReadInt("Введите m");
+int N = ReadInt("Введите n");
+positivNum(M, N);
+
+int ReadInt(string massage)
+{
+    System.Console.WriteLine(massage);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void positivNum(int arg1, int arg2)
+{
+    if (arg1 <= arg2)
+    {
+        return;
+    }
+    positivNum(arg2-1);
+    System.Console.WriteLine(arg2);
+    
+}
+
---
+++ P_9_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8 -> 30
+
+
+int m = ReadInt("Введите m");
+int n = ReadInt("Введите n");
+SumFromMToN(m, n);
+
+int ReadInt(string massage)
+{
+    System.Console.WriteLine(massage);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
